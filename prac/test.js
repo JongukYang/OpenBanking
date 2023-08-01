88ad60fd46d829ebe84d7ce5846c6e19b728bbfa
JongukYang <didwhddnr123@naver.com>
2023-08-01 11:28:09 +0900

    first example : 변수 선언

---
+++ prac/test.js
@@ -0,0 +1,12 @@
+var a = "ghdrlfehd";
+console.log(a);
+var a = "rlarlfehd";
+console.log(a);
+
+let b = 1000;
+let c = 2000;
+b = "hello";
+console.log(b);
+
+const con = "node.js";
+console.log(con);
