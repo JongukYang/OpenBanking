89f4342966990121645977ab333e3a60ecfe1b7c
JongukYang <didwhddnr123@naver.com>
2023-08-03 10:49:24 +0900

    잔액조회

--- fintech/src/pages/BalancePage.js
+++ fintech/src/pages/BalancePage.js
@@ -1,58 +1,85 @@
-// 이 페이지에서 계좌 거래 내역 조회 구현하기
-
-import React, { useEffect, useState } from "react";
-import AppHeader from "../components/common/AppHeader";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import axios from "axios";
+import AppHeader from "../components/common/AppHeader";
 import queryString from "query-string";
+import axios from "axios";
+import BalanceCard from "../components/balance/BalanceCard";
 
 const BalancePage = () => {
-  const [balance, setBalance] = useState("아직 없음");
-
-  const queryParams = useLocation().search;
-  console.log(queryParams);
-
-  const parsed = queryString.parse(queryParams);
-  console.log(parsed.fintechUseNo);
-
-  const fintechUseNum = parsed.fintechUseNo;
-  const bankTranId = "M202301659U000000001";
-
   let accessToken = "";
   let userSeqNo = "";
+  const [balance, setBalance] = useState("아직없음");
+
+  const queryParams = useLocation().search;
+  const parsed = queryString.parse(queryParams);
+  const fintechUseNum = parsed.fintechUseNo;
+
+  function generateRandom9DigitNumber() {
+    const min = 100000000; // Minimum value (smallest 9-digit number)
+    const max = 999999999; // Maximum value (largest 9-digit number)
+
+    const random9DigitNumber =
+      Math.floor(Math.random() * (max - min + 1)) + min;
+    return random9DigitNumber.toString();
+  }
+
+  const genTrasId = () => {
+    return "M202300440U" + generateRandom9DigitNumber();
+  };
 
   useEffect(() => {
     console.log(localStorage.getItem("accessToken"));
     console.log(localStorage.getItem("userSeqNo"));
+    console.log(fintechUseNum);
+    console.log(genTrasId());
     accessToken = localStorage.getItem("accessToken");
     userSeqNo = localStorage.getItem("userSeqNo");
-    getAccountBalance();
+    getBalance();
   }, []);
+  function getCurrentDateTime() {
+    const now = new Date();
 
-  const getAccountBalance = () => {
-    let requestOption = {
-      url: `/v2.0/account/balance/fin_num?bank_tran_id=${bankTranId}&fintech_use_num=${fintechUseNum}&tran_dtime=20230802161956`,
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    const currentDateTime = `${year}${month}${day}${hours}${minutes}${seconds}`;
+    return currentDateTime;
+  }
+
+  const getBalance = () => {
+    const sendObj = {
+      bank_tran_id: genTrasId(),
+      fintech_use_num: fintechUseNum,
+      tran_dtime: getCurrentDateTime(),
+    };
+
+    const option = {
       method: "GET",
+      url: "v2.0/account/balance/fin_num",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         Authorization: `Bearer ${accessToken}`,
       },
+      params: sendObj,
     };
-    // 가져오기
-    axios(requestOption)
-      .then(({ data }) => {
-        console.log(data);
-        setBalance(data.balance_amt);
-      })
-      .catch((err) => {
-        console.log("메인 실패");
-      });
+
+    axios(option).then(({ data }) => {
+      setBalance(data);
+    });
   };
 
   return (
     <div>
-      <AppHeader title={"잔액조회"} />
-      {balance}
+      <AppHeader title="잔액조회"></AppHeader>
+      <BalanceCard
+        bankName={balance.bank_name}
+        fintechNo={balance.fintech_use_num}
+        balance={balance.balance_amt}
+      ></BalanceCard>
     </div>
   );
 };
